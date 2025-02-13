e494356fffae8450106fce3fc9defd046fbbf329
Neeraj Kumar Verma <neerajjnv2015@gmail.com>
2025-02-13 21:55:23 +0530

    Banner

---
+++ frontened/src/components/Banner.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Banner = () => {
+  return (
+    <>
+    <div className='max-w-space-2xl container mx-auto md:px-20 px-4 flex flex-col md:flex-row' > 
+    <div className='w-full md:w-1.5'>Left</div>
+    <div className='w-full md:w-1.5'>Right</div>
+    </div>
+    </>
+  )
+}
+
+export default Banner
